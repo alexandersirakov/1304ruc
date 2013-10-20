5e589273b086b4e8ced1f621b788c1d53b96d991
alexandersirakov <alexmail39@gmail.com>
2013-10-20 17:14:42 +0300

    C#

---
+++ lection2/task8.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace task8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a;
+            double b;
+            double c;
+            double d;
+            double x1;
+            double x2;
+
+            Console.WriteLine("Enter a: ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter b: ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter c: ");
+            c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("{0}*x^2 + {1}*b + {2} = 0", a, b, c);
+            
+            d = b * b - 4 * a * c;
+            x1 = (-b + Math.Sqrt(d)) / a;
+            x2 = (-b - Math.Sqrt(d)) / a;
+
+            Console.WriteLine("X1= {0} and X2= {1}", x1, x2);
+            
+        }
+    }
+}
---
+++ lection2/task9.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace task9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int name;
+
+            Console.WriteLine("Enter a digit from 1 to 7.");
+            name = int.Parse(Console.ReadLine());
+
+            switch (name)
+            {
+                case 1: Console.WriteLine("Name of the day: Monday"); break;
+                case 2: Console.WriteLine("Name of the day: Tuesday"); break;
+                case 3: Console.WriteLine("Name of the day: Wednesday"); break;
+                case 4: Console.WriteLine("Name of the day: Thursday"); break;
+                case 5: Console.WriteLine("Name of the day: Friday"); break;
+                case 6: Console.WriteLine("Name of the day: Saturday"); break;
+                case 7: Console.WriteLine("Name of the day: Sunday"); break;
+            }
+
+        }
+    }
+}
